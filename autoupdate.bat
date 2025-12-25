41e8cd278fa917576f7763909fdd9ad92694e1e3
byte10101010 <favlines77@gmail.com>
2025-12-25 13:27:07 +0530

    Auto-update: Thu 12/25/2025 13:27:07.46

---
+++ autoupdate.bat
@@ -0,0 +1,15 @@
+@echo off
+:: Move to your repo folder
+cd /d "D:\git_volume\iptv_my"
+
+:: Run your Python script to update the M3U8 file
+python your_script.py
+
+:: Add the file to git
+git add .
+
+:: Commit the changes
+git commit -m "Auto-update: %date% %time%"
+
+:: Push to GitHub (This will now work without asking for a password)
+git push origin main
